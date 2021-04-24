a76e0ff92d449eab4d25a20dc8464a6eb694300a
PraveenKusuluri08 <praveenkusuluri07@gmail.com>
2021-04-24 16:39:08 +0530

    Signin component designed

--- src/components/authentication/signin/Presentation.js
+++ src/components/authentication/signin/Presentation.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react"
+import { Button, Card, Form } from "react-bootstrap"
 
-const Presentation = () => {
+const Presentation = (props) => {
+  const { handleChange, handleSubmit } = props
   return (
-    <div>
-      
+    <div className="container small center align">
+      <Card>
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" onhange={handleChange} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" onChange={handleChange} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
